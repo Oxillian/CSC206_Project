fe1ff7f5ff166d399e4433792ecde4321bdddcde
Oxillian <robbiedejulia@gmail.com>
2022-05-05 01:44:27 -0400

    Cleaning up

--- hi.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-# Online Python - IDE, Editor, Compiler, Interpreter
-
-base = 10
-step = 5
-numcells = 6
-upper_limit = base + (step * numcells)
-axes_2d = np.arange(base, upper_limit, step).reshape([2,3])
-print(axes_2d)
--- switchState.py
+++
@@ -1,9 +0,0 @@
-def numbers_to_strings(argument):
-    switcher = {
-        0: "zero",
-        1: "one",
-        2: "two",
-    }
-
-x=0
-print(x)
